a263d3e1cc944e24ff3fc354d2bf9777c541a789
Michael Sogos <michael.sogos@gurustudioweb.it>
2015-09-19 14:33:43 +0200

    Defined broker implementation with buffered stream

---
+++ PrimeBrick.MessageQueue.Server/Events.vb
@@ -0,0 +1,21 @@
+﻿Imports System.ComponentModel
+
+Public Class ServerLogEventArgs
+    Inherits AsyncCompletedEventArgs
+
+    Public ReadOnly Property Message As String
+    Public ReadOnly Property Gravity As String
+
+    Public Sub New(Message As String, Gravity As LogGravity)
+        MyBase.New(Nothing, False, Nothing)
+        Me.Message = Message
+        Me.Gravity = Gravity
+    End Sub
+
+End Class
+
+Public Enum LogGravity
+    [Error] = 0
+    Warning = 1
+    Information = 2
+End Enum
